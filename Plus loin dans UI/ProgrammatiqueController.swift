760a0a1854cdb58163f79ff98d18ad2308ff585d
Valentin SCHERER <valentinscherer@MBP-de-Valentin.fritz.box>
2018-08-25 16:33:48 +0200

    Uibouton

--- Plus loin dans UI/ProgrammatiqueController.swift
+++ Plus loin dans UI/ProgrammatiqueController.swift
@@ -13,6 +13,8 @@ class ProgrammatiqueController: UIViewController {
     
     var monPremierUiview: UIView?
     var monPremierLabel: UILabel?
+    var monPremierBouton: UIButton?
+    
     
     
     override func viewDidLoad() {
@@ -31,6 +33,34 @@ class ProgrammatiqueController: UIViewController {
         monPremierLabel?.font = UIFont(name: "Chalkduster", size: 20)
         monPremierLabel?.textAlignment = NSTextAlignment.center
         view.addSubview(monPremierLabel!)
+        
+        let rectDeMonBouton = CGRect(x: view.frame.width / 2 - 75, y: monPremierLabel!.frame.maxY + 20, width: 150, height: 40)
+        
+        monPremierBouton = UIButton(frame: rectDeMonBouton)
+        monPremierBouton?.setTitle("Appuyer", for: UIControlState.normal)
+        monPremierBouton?.tintColor = UIColor.red
+        monPremierBouton?.titleLabel?.font = UIFont.boldSystemFont(ofSize: 18)
+        monPremierBouton?.backgroundColor = UIColor.black
+        monPremierBouton?.layer.borderColor = UIColor.white.cgColor
+        monPremierBouton?.layer.borderWidth = 2
+        view.addSubview(monPremierBouton!)
+        
+        monPremierBouton?.addTarget(self, action: #selector(boutonAppuye), for: .touchUpInside)
+        
+    }
+    
+    @objc func boutonAppuye() {
+        
+        print("Tu as appuyé")
+        
+        
+    
     }
 
+    
+    
+    
+    
+    
+    
 }
